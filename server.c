d0a39e2eac9ce9c8be7a88377ddf715c2bd8efae
halt-spesn <ffoxy2870@gmail.com>
2025-12-15 14:02:23 +0200

    add ability to choose server port(example: ./server -p 4242)

--- server.c
+++ server.c
@@ -305,34 +305,90 @@ void *client_handler(void *arg) {
     return NULL;
 }
 
-int main() {
-    int server_fd, new_socket; struct sockaddr_in address;
-    signal(SIGPIPE, SIG_IGN); // Ignore broken pipe errors
+int main(int argc, char *argv[]) {
+    int server_fd, new_socket; 
+    struct sockaddr_in address;
+    
+    // 1. Set Default Port
+    int current_port = PORT; // Default 8888 from common.h
+
+    // 2. Parse Command Line Arguments
+    for (int i = 1; i < argc; i++) {
+        if (strcmp(argv[i], "-p") == 0 && i + 1 < argc) {
+            current_port = atoi(argv[i+1]);
+            if (current_port <= 0 || current_port > 65535) {
+                printf("Invalid port number. Using default %d.\n", PORT);
+                current_port = PORT;
+            }
+        }
+    }
+
+    signal(SIGPIPE, SIG_IGN); 
     init_game();
+    
     if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) exit(1);
-    int opt = 1; setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
-    address.sin_family = AF_INET; address.sin_addr.s_addr = INADDR_ANY; address.sin_port = htons(PORT);
-    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0) { perror("Bind failed"); exit(1); }
-    if (listen(server_fd, MAX_CLIENTS) < 0) { perror("Listen failed"); exit(1); }
-    printf("Server running on port %d...\n", PORT);
+    
+    int opt = 1; 
+    setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
+    
+    address.sin_family = AF_INET; 
+    address.sin_addr.s_addr = INADDR_ANY; 
+    
+    // 3. Use the variable port
+    address.sin_port = htons(current_port);
+    
+    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0) { 
+        perror("Bind failed"); 
+        exit(1); 
+    }
+    
+    if (listen(server_fd, MAX_CLIENTS) < 0) { 
+        perror("Listen failed"); 
+        exit(1); 
+    }
+    
+    printf("Server running on port %d...\n", current_port);
 
     // Start Tick Thread
     pthread_t ticker;
-    if (pthread_create(&ticker, NULL, tick_thread, NULL) != 0) { perror("Failed to start tick thread"); return 1; }
+    if (pthread_create(&ticker, NULL, tick_thread, NULL) != 0) { 
+        perror("Failed to start tick thread"); 
+        return 1; 
+    }
 
     while (1) {
         socklen_t addrlen = sizeof(address);
-        if ((new_socket = accept(server_fd, (struct sockaddr *)&address, &addrlen)) < 0) { perror("Accept failed"); continue; }
+        if ((new_socket = accept(server_fd, (struct sockaddr *)&address, &addrlen)) < 0) { 
+            perror("Accept failed"); 
+            continue; 
+        }
+        
         pthread_mutex_lock(&state_mutex);
         int slot = -1;
-        for (int i = 0; i < MAX_CLIENTS; i++) { if (client_sockets[i] == 0) { client_sockets[i] = new_socket; players[i].id = -1; slot = i; break; } }
+        for (int i = 0; i < MAX_CLIENTS; i++) { 
+            if (client_sockets[i] == 0) { 
+                client_sockets[i] = new_socket; 
+                players[i].id = -1; 
+                slot = i; 
+                break; 
+            } 
+        }
         pthread_mutex_unlock(&state_mutex);
+        
         if (slot != -1) {
-            int *arg = malloc(sizeof(int)); *arg = slot;
+            int *arg = malloc(sizeof(int)); 
+            *arg = slot;
             pthread_t thread_id;
-            if (pthread_create(&thread_id, NULL, client_handler, arg) != 0) { perror("Thread create failed"); close(new_socket); }
-            else pthread_detach(thread_id);
-        } else { printf("Server full.\n"); close(new_socket); }
+            if (pthread_create(&thread_id, NULL, client_handler, arg) != 0) { 
+                perror("Thread create failed"); 
+                close(new_socket); 
+            } else {
+                pthread_detach(thread_id);
+            }
+        } else { 
+            printf("Server full.\n"); 
+            close(new_socket); 
+        }
     }
     return 0;
 }
